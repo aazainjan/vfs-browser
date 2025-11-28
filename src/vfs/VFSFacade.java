1a263761b71a61c975366918c4e16f8f0e90dc1e
aazainjan <aazainjaan5@gmail.com>
2025-11-28 13:16:16 +0500

    Cleaned up code structure for better readability

--- src/vfs/VFSFacade.java
+++ src/vfs/VFSFacade.java
@@ -2,5 +2,5 @@ package vfs;
     // Currently acts as a placeholder; future helper methods could be added here.
 public class VFSFacade
 {
-    // Later methods for creating, moving, or opening files can be added to improve the code.
+    // Later other methods for creating, moving, or opening files can be added to improve the code.
 }
